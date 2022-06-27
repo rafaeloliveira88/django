65ef72e95433b145513515053935fde0151ee58d
rafaeloliveira88 <rafaeljmo88@gmail.com>
2022-06-27 19:10:57 +0100

    commit 19:10

--- exemplo/urls.py
+++ exemplo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie_stream.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movie_stream/', index),
 ]
